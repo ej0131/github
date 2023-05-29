4de4f4d0dd3e6b891b3116492316b36a1aa5e5ba
ej0131 <130959971+ej0131@users.noreply.github.com>
2023-05-29 14:03:52 +0800

    Update script.js

--- 111-suej/dist/script.js
+++ 111-suej/dist/script.js
@@ -1,1 +1,7 @@
 document.write("阿囉哈我是Su EJ ")
+var count = 0;
+    var counterElement = document.getElementById("counter");
+
+    function increment() {
+      count+=2;
+      counterElement.textContent = count; }
